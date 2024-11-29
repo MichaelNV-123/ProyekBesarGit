0a9d3f35e344daf32c7ef14c691a5ccccbd053d4
Nico <michaelnv04@gmail.com>
2024-11-29 09:23:55 +0700

    Menambahkan satu file index.php.

---
+++ index.php
@@ -0,0 +1,189 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="Ngesti Pandowo adalah perkumpulan kesenian tradisional Wayang Orang (WO) profesional berlokasi di Semarang. Saat ini, lokasi pentas Wayang Orang Ngesti Pandowo berada di Gedung Kesenian Ki Narto Sabdho dalam kompleks Taman Budaya Raden Saleh, Kota Semarang, Jawa Tengah, Indonesia. Ngesti Pandowo merupakan satu dari tiga perkumpulan kesenian tradisional Wayang Orang profesional yang bertahan di Indonesia, di samping Wayang Orang Sriwedari di Taman Sriwedari Solo dan Wayang Orang Bharata Jakarta.">
+    <meta name="author" content="Devcrud">
+    <title>NGESTI PANDOWO</title>
+ 
+    <link rel="stylesheet" href="public_html/assets/vendors/themify-icons/css/themify-icons.css">
+    <link rel="stylesheet" href="public_html/assets/css/index.css">
+
+</head>
+<body data-spy="scroll" data-target=".navbar" data-offset="40" id="home" class="dark-theme">
+    
+    <!--navbar-->
+    <nav class="page-navbar" data-spy="affix" data-offset-top="10">
+        <ul class="nav-navbar container">
+            <li class="nav-item"><a href="#" class="nav-link">Account</a></li>
+            <li class="nav-item"><a href="buyer_info.php" class="nav-link">Tiket</a></li>
+            <li class="nav-item"><a href="#" class="nav-link"><img src="public_html/assets/imgs/way.png" style="width: 80px; height: 80px;" alt=""></a></li>
+            <li class="nav-item"><a href="jadwal.html" class="nav-link">Jadwal</a></li>
+            <!-- Dropdown Navigation Item -->
+            <li class="nav-item dropdown">
+                <a href="#" class="nav-link dropdown-toggle" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Info
+                </a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <a class="dropdown-item" href="#sejarah_ngesti">Apa itu Ngesti Pandowo?</a>
+                    <a class="dropdown-item" href="jadwal.html">Kapan acara dimulai?</a>
+                    <a class="dropdown-item" href="buyer_info.php">Bagaimana cara membeli tiket?</a>
+                </div>
+            </li>
+        </ul>
+    </nav>
+
+    <div class="theme-selector">
+        <a href="javascript:void(0)" class="spinner">
+            <i class="ti-paint-bucket"></i>
+        </a>
+        <div class="body">
+            <a href="javascript:void(0)" class="light"></a>
+            <a href="javascript:void(0)" class="dark"></a>
+        </div>
+    </div>  
+    <!--header -->
+    <header class="header">
+        <div class="overlay"></div>
+        <div class="header-content">
+            <h1 class="header-title">   NGESTI PANDOWO</h1>
+            <p class="header-subtitle">Jl. Sriwijaya No.29, Tegalsari, Kec. Candisari, Kota Semarang, Jawa Tengah 50614</p>
+
+            <button class="btn btn-theme-color modal-toggle"><i class="ti-control-play text-danger"></i> TONTON CUPLIKAN</button>
+
+        </div>
+    </header>
+    <div class="modalBox">
+        <div class="modalBox-body">
+            <iframe width="100%" height="450px" class="border-0" 
+             src="https://www.youtube.com/embed/BlXW0hD_ucU?si=jhTouecugYK6rPcg&start=27" 
+            title="ngesti pandawa" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+            referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+
+        </div>          
+    </div>
+    
+    <div class="container page-container">
+        <div class="col-md-10 col-lg-8 m-auto">
+            <h6 class="title mb-4">Sejarah Ngesti Pandawa</h6>
+            <p class="mb-5">Ngesti Pandowo adalah perkumpulan kesenian tradisional Wayang Orang (WO) profesional berlokasi di Semarang. Saat ini, lokasi pentas Wayang Orang Ngesti Pandowo berada di Gedung Kesenian Ki Narto Sabdho dalam kompleks Taman Budaya Raden Saleh, Kota Semarang, Jawa Tengah, Indonesia. Ngesti Pandowo merupakan satu dari tiga perkumpulan kesenian tradisional Wayang Orang profesional yang bertahan di Indonesia, di samping Wayang Orang Sriwedari di Taman Sriwedari Solo dan Wayang Orang Bharata Jakarta.</p>
+            <p class="mb-5">Kelompok ini didirikan di Madiun oleh Ki Sastro Sabdho pada tanggal 1 Juli 1937 dengan tujuan untuk menghidupkan kembali
+            seni pertunjukan wayang orang panggung. Wayang Orang Panggung adalah kombinasi antara Wayang Orang Keraton (sering
+            disebut wayang orang pendhapa) dan teater barat. Keberadaan Ngesti Pandowo bertujuan untuk melestarikan seni wayang
+            orang serta menumbuhkan kecintaan terhadap seni tradisi. Pertunjukan wayang orang juga berfungsi sebagai hiburan bagi
+            masyarakat. Sejak awal berdirinya, Ngesti Pandowo telah mendapatkan sambutan positif dari masyarakat, tidak hanya dari
+            kalangan Jawa, tetapi juga dari orang-orang Belanda dan keturunan Tionghoa. Dengan demikian, wayang orang telah menjadi
+            seni pertunjukan yang dapat dinikmati oleh berbagai lapisan masyarakat, baik pribumi maupun non-pribumi.</p>
+        </div>  
+        <div>
+        <p><img src="public_html/assets/imgs/pendiri.png" alt="pendiri"></p>
+        </div>
+
+        <div class="row mb-4">
+            <div class="col-md-4">
+                <a href="javascript:void(0)" class="overlay-img">
+                    <img src="public_html/assets/imgs/nges1.jpg" alt="gambar2">  
+                    <div class="overlay"></div> 
+                    <div class="des">
+                        <h1 class="title">Ngesti Pandawa 2</h1>
+                        <h6 class="subtitle">Wayang</p>
+                        <p>Ngesti Pandowo adalah perkumpulan kesenian tradisional Wayang Orang.</p>
+                    </div>          
+                </a>
+            </div>
+            <div class="col-md-4">
+                <a href="javascript:void(0)" class="overlay-img">
+                    <img src="public_html/assets/imgs/nges2.jpg" alt="Gambar 3">  
+                    <div class="overlay"></div> 
+                    <div class="des">
+                        <h1 class="title">Ngesti Pandawa 3</h1>
+                        <h6 class="subtitle">nulla pariatur</p>
+                        <p>Ngesti Pandowo adalah perkumpulan kesenian </p>
+                    </div>          
+                </a>
+            </div>
+            <div class="col-md-4">
+                <a href="javascript:void(0)" class="overlay-img">
+                    <img src="public_html/assets/imgs/nges3.jpg" alt="gambar4">  
+                    <div class="overlay"></div> 
+                    <div class="des">
+                        <h1 class="title">Ngesti Pandawa 4</h1>
+                        <h6 class="subtitle">Ngesti</p>
+                        <p>Ngesti Pandawa</p>
+                    </div>          
+                </a>
+            </div>          
+        </div>
+                <div class="card-body">
+                    <h6 class="card-subtitle mb-2 text-muted">Tiket</h6>
+                    <h3 class="card-title">Beli Tiket Disini</h3>
+                </div>
+<div class="col-md-10 col-lg-8 m-auto">
+    <a href="buyer_info.php" class="card text-center shadow-sm">
+        <img src="public_html/assets/imgs/blok.png" class="card-img-top img-fluid w-50 mx-auto" alt="T">
+    </a>
+</div>
+
+
+        <div class="row mb-5">
+            <div class="col-md-6">
+                <a href="javascript:void(0)" class="card">
+                    <img src="public_html/assets/imgs/jadwal.png" class="card-img" alt="Jadwal">
+                    <div class="card-body">
+                        <h6 class="card-subtitle">Jadwal</h6>
+                        <h3 class="card-title">Jadwal</h3>
+                        <p>Contoh jadwal</p>
+                    </div>                  
+                </a>
+            </div>  
+            <div class="col-md-6">
+                <a href="javascript:void(0)" class="card">
+                    <img src="public_html/assets/imgs/instagram.png" class="card-img" alt="Instagram" width="500px">
+                    <div class="card-body">
+                        <h6 class="card-subtitle">Social media</h6>
+                        <h3 class="card-title">Social media</h3>
+                        <p>Social media</p>
+                    </div>                  
+                </a>
+            </div>  
+        </div>
+        
+        <a href="#">Postingan Kami<i class="ti-angle-double-right angle"></i></a>
+    </div> 
+    <div class="contact-section">
+        <div class="overlay"></div>
+  
+        <div class="container">
+            <div class="col-md-10 col-lg-8 m-auto">
+                <h6 class="title mb-2">Hubungi Kita</h6>
+                <p class="mb-5">Hubungi Kita Disini</p>
+                <form action="" class="form-group">
+                    <input type="text" size="50" class="form-control" placeholder="Nama" required>
+                    <input type="email" class="form-control" placeholder="Email"requried>
+                    <textarea name="comment" id="comment" rows="6"   class="form-control" placeholder="Kritik dan Saran"></textarea>
+                    <input type="submit" value="Kriim" class="form-control">
+                </form>
+            </div>
+
+            <!--footer-->
+            <footer class="footer">
+                <p class="infos">&copy; <script>document.write(new Date().getFullYear())</script>, Ngesti <i class="ti-heart text-danger"></i>  Pandawa</a></p>       
+                <span>|</span>  
+                <div class="links">
+                    <a href="https://www.instagram.com/ngestipandowo.official?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==">Instagram</a>
+                    <a href="buyer_info.php">Tiket</a>
+                    <a href="jadwal.html">Jadwal</a>
+                </div>
+            </footer>
+
+        </div>
+    </div>
+
+    <script src="public_html/assets/vendors/jquery/jquery-3.4.1.js"></script>
+    <script src="public_html/assets/vendors/bootstrap/bootstrap.bundle.js"></script>
+    <script src="public_html/assets/vendors/bootstrap/bootstrap.affix.js"></script>
+    <script src="public_html/assets/js/dorang.js"></script>
+
+</body>
+</html>
